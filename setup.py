c3f04cca4466d064117e8a89635156ff332fceb7
Dan Sinclair <dansinclair@me.com>
2016-07-04 16:14:26 +0100

    Fix MANIFEST.ini

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangorelishblog',
-    version='1.0.0a10',
+    version='1.0.0a11',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license

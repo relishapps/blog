98095901919efa7a360746a87ce62147b5c5fc3e
Dan Sinclair <dansinclair@me.com>
2016-07-04 15:20:33 +0100

    Update URLs

--- django-relish-blog/urls.py
+++ django-relish-blog/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from .views import Blog, BlogPost
 
 urlpatterns = [
-    url(r'^$', Blog.as_view(), name='blog'),
-    url(r'^([a-z0-9-]+)/$', BlogPost.as_view(), name='blog_post')
+    url(r'^blog/$', Blog.as_view(), name='blog'),
+    url(r'^blog/([a-z0-9-]+)/$', BlogPost.as_view(), name='blog_post')
 ]
